52e73b86ad2972805cd401d917b536510603e141
邓文浩 <aboutmydreams@163.com>
2019-07-10 01:12:16 +0800

    add data

---
+++ money/get_money_data.py
@@ -0,0 +1,55 @@
+import requests
+from bs4 import BeautifulSoup
+
+first_url = "https://fee.icbc.com.cn/index.jsp"
+second_url = "https://fee.icbc.com.cn/servlet/ICBCINBSReqServlet"
+
+bd_session = requests.Session()
+
+response = bd_session.get(first_url)
+cookii = requests.utils.dict_from_cookiejar(response.cookies)
+
+cookie = ""
+for i in dict(cookii):
+    cookie += (i+"=")+dict(cookii)[i]+";"
+
+
+
+headers = {
+    'Accept': 'application/json, text/javascript, */*; q=0.01',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'zh-CN,zh;q=0.9',
+    'Connection': 'keep-alive',
+    'Content-Length': '166',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'Cookie': cookie,
+    'Host': 'fee.icbc.com.cn',
+    'Origin': 'https://fee.icbc.com.cn',
+    'Referer': 'https://fee.icbc.com.cn/servlet/ICBCINBSReqServlet',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3833.0 Safari/537.36',
+    'X-Requested-With': 'XMLHttpRequest'
+}
+
+
+datas = {
+    'dse_sessionId': 'FTIMDLGAAOCICCEACIJPEXDECPAZIUIJCYJLJGEL',
+    'randomIdAppendToFormForMacSafari': '1562649689360-83023251',
+    'dse_applicationId': '-1',
+    'dse_operationName': 'per_SelfHelpPaymentOp',
+    'dse_pageId': '3',
+    'pageMark': '3',
+    'paymentContent': 'busiCode=2201804177',
+    'maskFlag': '0',
+    'IN_PAYITEMCODE': 'PJ120012021000015901',
+    'paymentR': '0',
+}
+
+res = requests.post(second_url,headers=headers,data=datas)
+soup = BeautifulSoup(res.text,"lxml")
+name = soup.select("#item20")
+money = soup.select("#item24")
+user_id = soup.select("#busiCode")
+
+print(res.text)
---
+++ money/get_user_data.py
@@ -0,0 +1,151 @@
+import requests,lxml,re,random
+import threading, time
+from bs4 import BeautifulSoup
+
+def get_user(user_id):
+    user_id = str(user_id)
+    url1 = "https://ssl.jxufe.edu.cn/uid/forget!forget2Type"
+
+    bd_session = requests.Session()
+
+    response = bd_session.get(url1)
+
+
+    url2 = "https://ssl.jxufe.edu.cn/uid/forget_json!forgetExistAccount"
+    data2 = {
+        'needAlert': 'false',
+        'needRedirect': 'true',
+        'accountkey': '{}'.format(user_id),
+    }
+    bd_session.post(url2, data=data2)
+    res = bd_session.get("https://ssl.jxufe.edu.cn/uid/forget!forget2Type").text
+    soup = BeautifulSoup(res, "lxml")
+    s_path = "div.input-div"
+    user_data = str(soup.select(s_path)).replace("\xa0","").replace('\n','').replace('\r','').replace('\t','')
+    so = ""
+    for i in list(user_data):
+        if (i != '') and (i != ' ') and (i != '"'):
+            so += i
+    # print(so)
+    my_data = re.findall('(?<=</span>).*?(?=</div>)', str(so))
+    all_list = [user_id]
+    data_list = list(i for i in my_data if len(i) < 12)
+    all_list.extend(data_list)
+    if len(all_list) > 0:
+        print(all_list)
+    return all_list
+
+
+# for user_id in range(2201704176,2201704186):
+#     a = get_user(str(user_id))
+#     print(a)
+# get_user(2201704176)
+
+
+
+a = []
+#
+
+def test1():
+    for i in range(2201705348,2201705448):
+        # id0 = random.randint(2201700000,2201799999)
+        res = get_user(i)
+        if res != []:
+            f = open("t1.txt",'a')
+            f.write(str(res)+'\n')
+            f.close()
+            a.append(i)
+
+
+def test2():
+    for i in range(2201705248,2201705348):
+        # id0 = random.randint(2201700000,2201799999)
+        res = get_user(i)
+        if res != []:
+            f = open("t1.txt", 'a')
+            f.write(str(res) + '\n')
+            f.close()
+            a.append(i)
+
+def test3():
+    for i in range(2201790001,2201790009):
+        # id0 = random.randint(2201700000,2201799999)
+        res = get_user(i)
+        if res != []:
+            f = open("t1.txt", 'a')
+            f.write(str(res) + '\n')
+            f.close()
+            a.append(i)
+
+
+def test4():
+    for i in range(2201780001,2201780009):
+        # id0 = random.randint(2201700000,2201799999)
+        res = get_user(i)
+        if res != []:
+            f = open("t1.txt", 'a')
+            f.write(str(res) + '\n')
+            f.close()
+            a.append(i)
+
+def test5():
+    for i in range(2201770001,2201770009):
+        # id0 = random.randint(2201700000,2201799999)
+        res = get_user(i)
+        if res != []:
+            f = open("t1.txt", 'a')
+            f.write(str(res) + '\n')
+            f.close()
+            a.append(i)
+
+def test6():
+    for i in range(2201760001,2201760009):
+        # id0 = random.randint(2201700000,2201799999)
+        res = get_user(i)
+        if res != []:
+            f = open("t1.txt", 'a')
+            f.write(str(res) + '\n')
+            f.close()
+            a.append(i)
+
+def test6():
+    for i in range(2201750001,2201750009):
+        # id0 = random.randint(2201700000,2201799999)
+        res = get_user(i)
+        if res != []:
+            f = open("t1.txt", 'a')
+            f.write(str(res) + '\n')
+            f.close()
+            a.append(i)
+def test7():
+    for i in range(2201740001,2201740009):
+        # id0 = random.randint(2201700000,2201799999)
+        res = get_user(i)
+        if res != []:
+            f = open("t1.txt", 'a')
+            f.write(str(res) + '\n')
+            f.close()
+            a.append(i)
+def last():
+    t1 = threading.Thread(target=test1)
+    t2 = threading.Thread(target=test2)
+    t3 = threading.Thread(target=test3)
+    t4 = threading.Thread(target=test4)
+    t5 = threading.Thread(target=test5)
+    t6 = threading.Thread(target=test6)
+    t7 = threading.Thread(target=test7)
+
+    # t1.start()
+    # t2.start()
+    t3.start()
+    t4.start()
+    t5.start()
+    t6.start()
+    t7.start()
+
+try:
+    last()
+except Exception as e:
+    print(e)
+    pass
+print(a)
